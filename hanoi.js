bdf885a8dd55f8fca3c10ee1eb9655b697f9b889
dfl <dfl.github@gmail.com>
2022-08-28 21:55:28 +0300

    Better demo code

--- hanoi.js
+++ hanoi.js
@@ -5,31 +5,34 @@ class Hanoi {
 	constructor() {
 		this.towers = [0, 0, 0];
 		this.delayMS = 1000;
+		this.numTiles = 0;
 	}
 
 	async go() {
-		for (let i = 0, len = this.towers[0]; i < len; ++i) {
-			this.moveOne(0, 1);
-			await(this.delay());
+		while (this.towers[0].length > 1) {
+			var from = this.randomIndex(),
+				to = this.randomIndex(from);
+			this.moveOne(from, to);
+			await (this.delay());
 		}
 	}
 
 	init() {
-		$("#ntowers").on("change", function(evt) {
+		$("#ntowers").on("change", function (evt) {
 			this.setNumberOfTiles($(evt.currentTarget).val())
 		}.bind(this))
-		.change();
-		$("#ndelay").on("change", function(evt) {
+			.change();
+		$("#ndelay").on("change", (evt) => {
 			var d = $(evt.currentTarget).val();
 			if (!isNaN(d)) {
-				this.delayMS = Math.max(0.1, Math.min(d, 2)) * 1000;
+				this.delayMS = Math.max(0.1, Math.min(Number(d), 2)) * 1000;
 			}
 		})
-		.change();
+			.change();
 
-		$("#btn-go").click(function() {
+		$("#btn-go").click(() => {
 			this.go();
-		}.bind(this));
+		});
 	}
 
 	setNumberOfTiles(n) {
@@ -39,16 +42,35 @@ class Hanoi {
 		else {
 			n = Math.max(MIN_TILES, Math.min(n, MAX_TILES));
 		}
-		this.towers = [Number(n), 0, 0];
+		this.numTiles = n;
+		this.towers = [[], [], []];
+		for (let i = 0; i < n; ++i) {
+			this.towers[0].push(n - i - 1);
+		}
 		this.render();
+		$(".towers").css("min-height", `${n * 30}px`);
 	}
 
 	moveOne(from, to) {
-		if (this.towers[from] > 0) {
-			this.towers[from]--;
-			this.towers[to]++;
-			this.render();
+		var toMove = this.lastItem(from);
+		var atMove = this.lastItem(to);
+		if (toMove !== null) {
+			if (atMove !== null && toMove > atMove) {
+				console.warn(`Cannot move tile ${toMove} above tile ${atMove}`);
+				return;
+			}
+			this.towers[from].pop();
+			this.towers[to].push(toMove);
+			this.renderTower(from);
+			this.renderTower(to);
 		}
+	}
+
+	lastItem(n) {
+		var arr = this.towers[n];
+		var len = arr.length;
+		return len === 0 ?
+			null : arr[len - 1];
 	}
 
 	render() {
@@ -60,8 +82,11 @@ class Hanoi {
 	renderTower(ind) {
 		var $t = $(`.tower[data-index="${ind}"]`);
 		$t.find(".tile").remove();
-		for (let i = 0; i < this.towers[ind]; ++i) {
-			$t.prepend($(`<div class="tile"></div>`));
+		var arr = this.towers[ind];
+		for (let i = 0; i < arr.length; ++i) {
+			var current = arr[i];
+			var percent = 100 - (this.numTiles - current - 1) * 5;
+			$t.prepend($(`<div class="tile" data-index="${current}" style="width: ${percent}%"></div>`));
 		}
 	}
 
@@ -69,6 +94,12 @@ class Hanoi {
 		return new Promise((resolve) => {
 			setTimeout(resolve, this.delayMS);
 		})
+	}
+
+	randomIndex(toAvoid) {
+		var rand = Math.random();
+		var ret = rand < 0.33 ? 0 : rand < 0.66 ? 1 : 2;
+		return ret === toAvoid ? this.randomIndex(toAvoid) : ret;
 	}
 
 
